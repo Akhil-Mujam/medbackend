623a62050b3155958a658d836f825d9dac77eae5
Akhil-Mujam <akhilmujam@gmail.com>
2023-04-30 21:25:38 +0530

    backend

---
+++ health/backend/Database/upload.js
@@ -0,0 +1,28 @@
+
+
+const mongoose = require('mongoose')
+
+const upschema = new mongoose.Schema({
+
+    Title:{
+        type:String,
+        required:true
+    },
+    Notes:{
+        type:String,
+        required:false
+    },
+    img:{
+        type:String,
+        required:true
+    },
+    date:{
+    type:Date,
+    default:Date.now()
+    }
+    
+})
+
+const upmodel =  mongoose.model('upload',upschema);
+
+module.exports = upmodel;
